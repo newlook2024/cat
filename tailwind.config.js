9742423c4fe09fea7e482e6e3aca498438407e6e
newlook2024 <abdulvahob2022@gmail.com>
2024-05-31 11:20:54 +0500

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black1: '#1A212F'
+      },
+      fontSize: {
+        3.5: '14px'
+      },
+      margin: {
+        17.5: '70px'
+      },
+      backgroundColor:{
+        orange: '#FAC663'
+      },
+      padding:{
+        17.5: '70px'
+      }
+    },
+  },
+  plugins: [],
+}
+
